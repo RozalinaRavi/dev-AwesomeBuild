6ad76d3cf085e39add139e6bd044b0c4e3b7fe41
RozalinaRavi <violet_star@ukr.net>
2024-05-14 20:20:32 +0300

    write functions for builds and floors

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,15 @@
-  
+const genplanItem = document.querySelectorAll('.genplan-item')
+const cellAdress = document.querySelector('#adress')
+const cellFloors = document/querySelector('#floors')
+
+
+genplanItem.forEach(build => {
+    build.addEventListener('mouseover', () => {
+        const thisAdress = build.getAttribute('data-adress')
+        const thisFloors = build.getAttribute('data-floors')
+
+
+        cellAdress.innerHTML = thisAdress
+        cellFloors.innerHTML = thisFloors
+    })
+})
