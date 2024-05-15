688478741f3c6de4f4ae6ac8ed84af5bbbfbd39f
RozalinaRavi <violet_star@ukr.net>
2024-05-16 00:10:06 +0300

    create build-item-page

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,15 +1,18 @@
-const genplanItem = document.querySelectorAll('.genplan-item')
+const genplanItem = document.querySelectorAll('.genplan-item-path')
 const cellAdress = document.querySelector('#adress')
-const cellFloors = document/querySelector('#floors')
+const cellFloors = document.querySelector('#floors')
+const cellFreeFlats = document.querySelector('#freeFlats')
 
 
 genplanItem.forEach(build => {
     build.addEventListener('mouseover', () => {
         const thisAdress = build.getAttribute('data-adress')
         const thisFloors = build.getAttribute('data-floors')
+        const thisFreeFlats = build.getAttribute('data-free-flats')
 
 
         cellAdress.innerHTML = thisAdress
         cellFloors.innerHTML = thisFloors
+        cellFreeFlats.innerHTML = thisFreeFlats
     })
 })
