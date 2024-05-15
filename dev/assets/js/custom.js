ee53c080064499dce227bf0b4b67d668ac2586b9
RozalinaRavi <violet_star@ukr.net>
2024-05-16 01:37:43 +0300

    added picture for build-item page

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -9,10 +9,30 @@ genplanItem.forEach(build => {
         const thisAdress = build.getAttribute('data-adress')
         const thisFloors = build.getAttribute('data-floors')
         const thisFreeFlats = build.getAttribute('data-free-flats')
+        
 
 
         cellAdress.innerHTML = thisAdress
         cellFloors.innerHTML = thisFloors
         cellFreeFlats.innerHTML = thisFreeFlats
+    
     })
+
+        const buildFreeFlats = build.getAttribute('data-free-flats') 
+
+        if (buildFreeFlats === "0") {
+            build.classList.add('sold')
+        }
+
+
+
+
+        if (build.classList.contains ('sold')) {
+            const buildLink = build.closest('.genplan-item-link')
+
+            buildLink.addEventListener('click', (event) => {
+                event.preventDefault()
+            })
+        }
 })
+
